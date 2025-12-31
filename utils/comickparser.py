fca3b7f1b2822b9fd02d8761955637025463b5f9
asmrokon <rokoncodes@gmail.com>
2025-12-31 17:04:40 +0600

    Removed print line

--- utils/comickparser.py
+++ utils/comickparser.py
@@ -13,8 +13,7 @@ async def get_manga_info_from_comick(slug):
     url = f"https://api.comick.dev/comic/{slug}"
     try:
         data = await get_comick_data(url)
-        print(json.dumps(data,indent=4))
-        # return True, data
+        return True, data
     except Exception as e:
         return False, e
 
